079a685ed39045b6ff88ad8f83bb673a1a5020d6
Sumeet Agarwal <sumeet.a@gmail.com>
2012-07-22 05:29:10 -0700

    alphabetize and write imports.

--- import8ion.py
+++ import8ion.py
@@ -24,3 +24,27 @@ class ExtractsImports(object):
         elif isinstance(node, ast.ImportFrom):
             for name in node.names:
                 yield make_import(node.module, name.name)
+
+
+class AlphabetizesImports(object):
+
+    @classmethod
+    def alphabetize(cls, imports):
+        return sorted(imports)
+
+
+class WritesImports(object):
+
+    @classmethod
+    def write(cls, imports):
+        return '\n'.join(cls._write(import_) for import_ in imports)
+
+    @classmethod
+    def _write(cls, import_):
+            return ('from %s import %s' % (import_.module, import_.name) if
+                    import_.name else 'import %s' % import_.module)
+
+
+def organize(code):
+    return WritesImports.write(
+        AlphabetizesImports.alphabetize(ExtractsImports.extract(code)))
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from expecter import expect
 
+from import8ion import AlphabetizesImports
 from import8ion import ExtractsImports
 from import8ion import Import
-
+from import8ion import WritesImports
+from import8ion import make_import
+from import8ion import organize
 
 
 unorganized_imports = """\
@@ -42,3 +45,25 @@ class ExtractsImportsTest(object):
         imports = ExtractsImports.extract('import name\n'
                                           'from module import name')
         expect(imports) == [Import('name', None), Import('module', 'name')]
+
+
+class WritesImportsTest(object):
+
+    def test_writes_imports(self):
+        imports = [make_import('b'), make_import('a', 'name')]
+        expect(WritesImports.write(imports)) == 'import b\nfrom a import name'
+
+
+class AlphabetizesImportsTest(object):
+
+    def test_uppercase_comes_before_lowercase(self):
+        imports = [make_import('b'), make_import('a'), make_import('C')]
+        expect(AlphabetizesImports.alphabetize(imports)) == [make_import('C'),
+                                                             make_import('a'),
+                                                             make_import('b')]
+
+    def test_periods_come_after(self):
+        imports = [make_import('a.b'), make_import('a.c'), make_import('a')]
+        expect(AlphabetizesImports.alphabetize(imports)) == [make_import('a'),
+                                                             make_import('a.b'),
+                                                             make_import('a.c')]
